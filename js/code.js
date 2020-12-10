d6e9d06fbad2fdccea447f0410f8b86046fb1391
Joses Kang <joseskang@gmail.com>
2020-12-10 13:19:13 +0800

    added date, account and child object templates

--- js/code.js
+++ js/code.js
@@ -1,17 +1,60 @@
 //Code for tribe
+//Global Variables
+var currentDate = new DateObject (new Date());
+
+//Object Templates
+//Date Object Template
+function DateObject (dateObj){
+    this.dateObj = dateObj;
+    this.dayOfTheWeek = function(){
+        var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var dayIndex = this.dateObj.getDay();
+        return dayArray[dayIndex];
+    };
+    this.day = dateObj.getDate();
+    this.month = dateObj.getMonth();
+    this.year = dateObj.getFullYear();
+    this.date = function(){
+        var dayStr = this.day.toString();
+        var monthStr = this.month.toString();
+        var yearStr = this.year.toString();
+
+        var dateReturn = parseInt(dayStr + monthStr + yearStr);
+
+        return dateReturn;
+    };
+}
+
+//Account Object Template
+function UserAccount (id, username, password, email, firstName, lastName, child){
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.child = child;
+}
+
+//Child Object Template
+function Child (id, name, dob, age, weight, height){
+    this.id = id;
+    this.name = name;
+    this.dob = dob;
+    this.age = currentDate
+}
 
 //Journal Object Template
-
 function JorunalEntry (entryName, entryDate, entryAge, entryText, entryMedia){
     this.entryName = entryName;
     this.entryDate = entryDate;
     this.Age = function (){
 
         //Find difference in years
-        var yearDiff = ParseInt(entryDate.slice(4,8)) - ParseInt(childDOB.slice(4,8));
+        var yearDiff = entryDate.year - childDOB.year;
 
         //Find difference in years
-        var monthDiff = ParseInt(entryDate.slice(2,4)) - ParseInt(childDOB.slice(2,4));
+        var monthDiff = entryDate.month - childDOB.month;
 
         //If month in entryDate is before month in childDOB
         if(monthDiff < 0){
