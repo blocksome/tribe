edb89a5d25944c77fb5a3cf68809243319da5a74
Joses Kang <joseskang@gmail.com>
2020-12-13 22:06:15 +0800

    Added edit functionality for journal entries

--- js/code.js
+++ js/code.js
@@ -15,6 +15,9 @@ var localAccount;
 //Journal Entry Array (1 user)
 var entryArray = [];
 
+//Current Journal Entry
+var currentEntry;
+
 //Current Date
 var currentDate = new DateObject(new Date());
 
@@ -47,15 +50,15 @@ $(document).ready(function () {
         alert.header = 'Hey!';
         alert.buttons = ['OK'];
 
-        if ($("#login-email:nth-child(2)").val() == "") {
-            alert.message = "Please provide your login email";
+        if ($("#login-email").val() == "") {
+            alert.message = "Please provide your login email.";
 
             document.body.appendChild(alert);
             return alert.present();
         }
 
-        else if ($("#login-password:nth-child(3)").val() == "") {
-            alert.message = "Please provide your login password";
+        else if ($("#login-password").val() == "") {
+            alert.message = "Please provide your login password.";
 
             document.body.appendChild(alert);
             return alert.present();
@@ -63,6 +66,46 @@ $(document).ready(function () {
 
         else {
             displayLoader("login");
+        }
+    });
+
+    //Register Button
+    $("body").on("click", "#register-btn1", function () {
+        const alert = document.createElement('ion-alert');
+        alert.cssClass = 'tribe-alert';
+        alert.header = 'Hey!';
+        alert.buttons = ['OK'];
+
+        if ($("#register-email").val() == "") {
+            alert.message = "You can't leave your email empty.";
+
+            document.body.appendChild(alert);
+            return alert.present();
+        }
+
+        else if ($("#register-password").val() == "") {
+            alert.message = "You can't leave your password empty.";
+
+            document.body.appendChild(alert);
+            return alert.present();
+        }
+
+        else if ($("#register-first-name").val() == "") {
+            alert.message = "You can't leave your first name empty.";
+
+            document.body.appendChild(alert);
+            return alert.present();
+        }
+
+        else if ($("#register-last-name").val() == "") {
+            alert.message = "You can't leave your last name empty.";
+
+            document.body.appendChild(alert);
+            return alert.present();
+        }
+
+        else {
+            displayLoader("register");
         }
     });
 
@@ -100,10 +143,10 @@ $(document).ready(function () {
         alert.buttons = ['OK'];
 
         //Storing input values as variables
-        var accountFirstName = $("#edit-account-firstname:nth-child(2)").val();
-        var accountLastName = $("#edit-account-lastname:nth-child(2)").val();
-        var accountEmail = $("#edit-account-email:nth-child(2)").val();
-        var accountPassword = $("#edit-account-password:nth-child(2)").val();
+        var accountFirstName = $("#edit-account-firstname").val();
+        var accountLastName = $("#edit-account-lastname").val();
+        var accountEmail = $("#edit-account-email").val();
+        var accountPassword = $("#edit-account-password").val();
 
         //Verify if entry name is blank  
         if (accountFirstName == "") {
@@ -171,11 +214,11 @@ $(document).ready(function () {
         alert.buttons = ['OK'];
 
         //Storing input values as variables
-        var childName = $("#edit-child-name:nth-child(2)").val();
-        var childDOB = $("#edit-child-dob:nth-child(2)").val();
-        var childGender = $("#edit-child-gender:nth-child(2)").val();
-        var childWeight = $("#edit-child-weight:nth-child(2)").val();
-        var childHeight = $("#edit-child-height:nth-child(2)").val();
+        var childName = $("#edit-child-name").val();
+        var childDOB = $("#edit-child-dob").val();
+        var childGender = $("#edit-child-gender").val();
+        var childWeight = $("#edit-child-weight").val();
+        var childHeight = $("#edit-child-height").val();
 
         //Verify if entry name is blank  
         if (childName == "") {
@@ -221,7 +264,7 @@ $(document).ready(function () {
 
             var submitDate = new Date(`${childDOB.slice(0, 4)}, ${childDOB.slice(5, 7)}, ${childDOB.slice(8, 10)}`);
             localAccount.child.dob = new DateObject(submitDate);
-            
+
             localAccount.child.gender = childGender;
             localAccount.child.weight = childWeight;
             localAccount.child.height = childHeight;
@@ -249,9 +292,9 @@ $(document).ready(function () {
     $("body").on("click", "#journal-submit", function () {
 
         //Storing input values as variables
-        var journalName = $("#journal-name:nth-child(2)").val();
-        var journalDetails = $("#journal-details:nth-child(2)").val();
-        var journalImage = $("#journal-image:nth-child(2)").val();
+        var journalName = $("#journal-name").val();
+        var journalDetails = $("#journal-details").val();
+        var journalImage = $("#journal-image").val();
 
         //Verify if entry name is blank  
         if (journalName == "") {
@@ -284,6 +327,56 @@ $(document).ready(function () {
                     console.log(accountsArray);
 
                     displayLoader("new-entry");
+
+                    //Sending data to database
+                    updateData(accountsArray);
+                }
+            }
+
+
+        }
+    });
+
+    //Journal Edit
+    $("body").on("click", "#journal-edit", function () {
+
+        //Storing input values as variables
+        var journalName = $("#journal-name").val();
+        var journalDetails = $("#journal-details").val();
+        var journalImage = $("#journal-image").val();
+
+        //Verify if entry name is blank  
+        if (journalName == "") {
+            const alert = document.createElement('ion-alert');
+            alert.cssClass = 'tribe-alert';
+            alert.header = 'Hey!';
+            alert.message = "You can't leave the entry name empty.";
+            alert.buttons = ['OK'];
+
+            document.body.appendChild(alert);
+            return alert.present();
+        }
+
+        //Submit new entry
+        else {
+
+            //Updating local user
+            var updatedEntry = new JournalEntry(journalName, currentDate, journalDetails, journalImage);
+
+            localAccount.journalEntries[currentEntry] = updatedEntry;
+            updateLocalData(localAccount);
+
+            console.log(localAccount.journalEntries);
+
+            //Updating local user in the account data
+            //Look for user in array
+            for (var i = 0; i < accountsArray.length; i++) {
+                if (accountsArray[i].id == localAccount.id) {
+                    accountsArray.splice(i, 1, localAccount);
+
+                    console.log(accountsArray);
+
+                    displayLoader("update-entry");
 
                     //Sending data to database
                     updateData(accountsArray);
@@ -351,10 +444,10 @@ function updateLocalData(updatedAccount) {
 function getLocalData() {
     dataGet = JSON.parse(localStorage.getItem("storedLogin"));
     if (dataGet) {
-        
+
         localChild = new Child(dataGet.child.id, dataGet.child.name, dataGet.child.dob, dataGet.child.gender, dataGet.child.weight, dataGet.child.height);
         localAccount = new UserAccount(dataGet.id, dataGet.email, dataGet.password, dataGet.firstName, dataGet.lastName, localChild, dataGet.journalEntries);
-        
+
         console.log(localChild);
         loadApp();
     }
@@ -439,15 +532,14 @@ function login() {
 
     //Run login details through all accounts in database
     for (var i = 0; i < accountsArray.length; i++) {
-        if ($("#login-email:nth-child(2)").val() == accountsArray[i].email) {
+        if ($("#login-email").val() == accountsArray[i].email) {
 
-            if ($("#login-password:nth-child(3)").val() == accountsArray[i].password) {
+            if ($("#login-password").val() == accountsArray[i].password) {
                 //This path is for successful login
                 //Stringify accountsArray
                 localAccount = accountsArray[i];
                 updateLocalData(localAccount);
 
-                //NOTE: experiment with location.replace and windows.location
                 loadApp();
             }
 
@@ -475,6 +567,33 @@ function login() {
     }
 
     $("#login-password:nth-child(2)").val();
+}
+
+//User Register Function
+function register() {
+    var newEmail = $("#register-email").val();
+    var newPassword = $("#register-password").val();
+    var newFirstName = $("#register-first-name").val();
+    var newLastName = $("#register-last-name").val();
+
+    var templateChild = new Child(0, "Child Name", currentDate, "Gender", "Weight (kg)", "Height (cm)");
+
+    var newAccount = new UserAccount(accountsArray.length, newEmail, newPassword, newFirstName, newLastName, templateChild, []);
+
+    accountsArray.push(newAccount);
+
+    updateData(accountsArray);
+    localAccount = newAccount;
+    updateLocalData(localAccount);
+
+    //Alert for successful registration
+    const alert = document.createElement('ion-alert');
+    alert.cssClass = 'tribe-alert';
+    alert.header = 'Yay!';
+    alert.buttons = ['OK'];
+
+
+    loadApp();
 }
 
 //Asynchronous loading function
@@ -543,16 +662,35 @@ async function displayLoader(reason) {
     }
 
     else if (reason == "new-entry") {
-        //Make sure 
 
         //Present Success Alert
         const alert = document.createElement('ion-alert');
         alert.cssClass = 'tribe-alert';
         alert.header = 'Done!';
-        alert.message = "New journal entry added, yay!";
+        alert.message = "New scrapbook entry added, yay!";
         alert.buttons = [{
             text: 'OK',
             handler: () => {
+                dismissJournalModal();
+                displayLoader("load-profile");
+            }
+        }];
+
+        document.body.appendChild(alert);
+        return alert.present();
+    }
+
+    else if (reason == "update-entry") {
+
+        //Present Success Alert
+        const alert = document.createElement('ion-alert');
+        alert.cssClass = 'tribe-alert';
+        alert.header = 'Done!';
+        alert.message = "Scrapbook entry updated, yay!";
+        alert.buttons = [{
+            text: 'OK',
+            handler: () => {
+                dismissEditJournalModal();
                 displayLoader("load-profile");
             }
         }];
@@ -564,38 +702,57 @@ async function displayLoader(reason) {
     else if (reason == "load-profile") {
         //Clear profile
         $(".profile-item").remove();
+        $(".profile-item-empty").remove();
 
         //Add account details
         $("#profile-name").text(localAccount.child.name);
         $("#profile-age").text(localAccount.child.age());
 
         //Add entries one by one
-        for (var i = 0; i < localAccount.journalEntries.length; i++) {
-            //If there was no image attached
-            if (localAccount.journalEntries[i].entryMedia == "" || localAccount.journalEntries[i].entryMedia == null) {
-                $("#profile-body").after(`
-                <ion-item class="profile-item">
+        if (localAccount.journalEntries.length > 0) {
+            for (var i = 0; i < localAccount.journalEntries.length; i++) {
+                //If there was no image attached
+                if (localAccount.journalEntries[i].entryMedia == "" || localAccount.journalEntries[i].entryMedia == null) {
+                    $("#profile-body").after(`
+                <ion-item class="profile-item" id="${i}">
             <ion-card class="profile-card">
                 <img src="" />
                 <ion-card-header>
                     <ion-card-subtitle>${localAccount.journalEntries[i].entryDate.dateObj}</ion-card-subtitle>
                     <ion-card-title>${localAccount.journalEntries[i].entryName}</ion-card-title>
                 </ion-card-header>
+            </ion-card>
+            </ion-item>
+            `);
+                }
+
+                else {
+                    //do stuff here if there's an img
+                }
+
+
+            }
+        }
+
+        //Feedback if there aren't any entries
+        else {
+            $("#profile-body").after(`
+            <ion-item class="profile-item-empty">
+            <ion-card class="profile-card" id="journal-entry-${i}">
+                <img src="" />
+                <ion-card-header>
+                    <ion-card-title>You've got no entries!</ion-card-title>
+                    <ion-card-subtitle>Oops</ion-card-subtitle>
+                </ion-card-header>
 
                 <ion-card-content>
-                    ${localAccount.journalEntries[i].entryText}
+                    Try adding one with the plus button down there!
                 </ion-card-content>
             </ion-card>
             </ion-item>
             `);
-            }
-
-            else {
-                //do stuff here if there's an img
-            }
-
-
         }
+
 
         isProfileLoaded = true;
     }
@@ -634,7 +791,7 @@ function DateObject(dateObj) {
     */
 
     this.year = dateObj.getFullYear();
-    
+
     /*
     this.date = function () {
         //Convert int to str for concantenation
